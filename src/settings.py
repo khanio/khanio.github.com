fe39c54c28456a635b8fbfdc02a2cf4579008463
Zakiullah Khan Mohammed <mail@khanio.com>
2013-04-10 19:50:11 +0800

    Updated Disqus sitename

--- src/settings.py
+++ src/settings.py
@@ -46,7 +46,7 @@ PAGE_DIR = ('pages/')
 ARTICLE_DIR = ('posts/')
 
 GOOGLE_ANALYTICS = 'UA-40023432-1'
-DISQUS_SITENAME = 'khan-io'
+DISQUS_SITENAME = 'khanio'
 TWITTER_USERNAME = 'khan_io'
 GITHUB_USERNAME = 'khanio'
 GOOGLE_PLUS_LINK = ''
